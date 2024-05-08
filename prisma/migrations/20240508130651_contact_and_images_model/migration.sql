58a7da0d5c8243cad14712968a2e72ebdb76190d
AmanRaza78 <razakhanaman9@gmail.com>
2024-05-08 18:37:23 +0530

    property listing form completed

---
+++ prisma/migrations/20240508130651_contact_and_images_model/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "PropertyImage" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "url" TEXT NOT NULL,
+    "propertyId" INTEGER NOT NULL,
+    CONSTRAINT "PropertyImage_propertyId_fkey" FOREIGN KEY ("propertyId") REFERENCES "Property" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Contact" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "propertyId" INTEGER NOT NULL,
+    CONSTRAINT "Contact_propertyId_fkey" FOREIGN KEY ("propertyId") REFERENCES "Property" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Contact_propertyId_key" ON "Contact"("propertyId");
--- src/app/components/addPropertyForm.tsx
+++ src/app/components/addPropertyForm.tsx
@@ -1,7 +1,17 @@
 "use client";
-import React from "react";
-import { Card, Checkbox, Input, Select, SelectItem, Textarea } from "@nextui-org/react";
+import React, { useState } from "react";
+import {
+  Card,
+  CardHeader,
+  Checkbox,
+  Input,
+  Select,
+  SelectItem,
+  Textarea,
+} from "@nextui-org/react";
 import { PropertyStatus, PropertyType } from "@prisma/client";
+import FileInput from "./fileUpload";
+import PictureCard from "./pictureCard";
 
 interface Props {
   types: PropertyType[];
@@ -9,51 +19,91 @@ interface Props {
 }
 
 const AddPropertyForm = (props: Props) => {
+  const [images, setImages] = useState<File[]>([]);
   return (
     <>
-    <Card className="p-4 grid grid-cols-1 md:grid-cols-3 m-6 gap-x-2">
-      <Input label="Property Name" className="md:col-span-3 mb-4" />
-      <Textarea label="Description" className="md:col-span-3 mb-4" />
+      <Card className="p-4 grid grid-cols-1 md:grid-cols-3 m-6 gap-x-2">
+        <CardHeader>
+          <h1 className="text-md font-semibold">Basic Information</h1>
+        </CardHeader>
+        <Input label="Property Name" className="md:col-span-3 mb-4" />
+        <Textarea label="Description" className="md:col-span-3 mb-4" />
 
-      <Select label="Property Type" selectionMode="single">
-        {props.types.map((item) => (
-          <SelectItem key={item.id} value={item.id}>
-            {item.value}
-          </SelectItem>
-        ))}
-      </Select>
+        <Select label="Property Type" selectionMode="single">
+          {props.types.map((item) => (
+            <SelectItem key={item.id} value={item.id}>
+              {item.value}
+            </SelectItem>
+          ))}
+        </Select>
 
-      <Select label="Property Status" selectionMode="single">
-        {props.status.map((item) => (
-          <SelectItem key={item.id} value={item.id}>
-            {item.value}
-          </SelectItem>
-        ))}
-      </Select>
-      <Input label="Price"/>
-    </Card>
+        <Select label="Property Status" selectionMode="single">
+          {props.status.map((item) => (
+            <SelectItem key={item.id} value={item.id}>
+              {item.value}
+            </SelectItem>
+          ))}
+        </Select>
+        <Input label="Price" />
+      </Card>
 
-    <Card className="p-4 grid grid-cols-1 md:grid-cols-3 m-6 gap-x-2">
-        <Textarea label="Street Address" className="md:col-span-3 mb-4"/>
-        <Input label="City" className="md:col-span-3 mb-4"/>
-        <Input label="State" className="md:col-span-3 mb-4"/>
-        <Input label="Country"/>
-        <Input label="Zip"/>
-        <Input label="Landmark"/>
-    </Card>
+      <Card className="p-4 grid grid-cols-1 md:grid-cols-3 m-6 gap-x-2">
+        <CardHeader>
+          <h1 className="text-md font-semibold">Location</h1>
+        </CardHeader>
+        <Textarea label="Street Address" className="md:col-span-3 mb-4" />
+        <Input label="City" className="md:col-span-3 mb-4" />
+        <Input label="State" className="md:col-span-3 mb-4" />
+        <Input label="Country" />
+        <Input label="Zip" />
+        <Input label="Landmark" />
+      </Card>
 
-    <Card className="p-4 grid grid-cols-1 md:grid-cols-2 m-6 gap-x-2">
-        <Input label="Bedrooms" className="mb-4"/>
-        <Input label="Bathrooms" className="mb-4"/>
-        <Input label="Parking Spots" className="mb-4"/>
-        <Input label="area" className="mb-4"/>
+      <Card className="p-4 grid grid-cols-1 md:grid-cols-2 m-6 gap-x-2">
+        <CardHeader className="col-span-2">
+          <h1 className="text-md font-semibold">Features</h1>
+        </CardHeader>
+        <Input label="Bedrooms" className="mb-4" />
+        <Input label="Bathrooms" className="mb-4" />
+        <Input label="Parking Spots" className="mb-4" />
+        <Input label="area" className="mb-4" />
         <div className="flex justify-between">
-        <Checkbox>Has Swimming Pool</Checkbox>
-        <Checkbox>Has Garden Yard</Checkbox>
-        <Checkbox>Has Balcony</Checkbox>
+          <Checkbox>Has Swimming Pool</Checkbox>
+          <Checkbox>Has Garden Yard</Checkbox>
+          <Checkbox>Has Balcony</Checkbox>
         </div>
+      </Card>
 
-    </Card>
+      <Card className="p-4 grid grid-cols-1 md:grid-cols-3 m-6 gap-x-2">
+        <CardHeader className="col-span-3">
+          <h1 className="text-md font-semibold">Upload Pictures</h1>
+        </CardHeader>
+        <FileInput
+          onSelect={(e) => setImages([(e as any).target.files[0], ...images])}
+          className="col-span-3"
+        />
+        <div className="flex gap-3 flex-wrap">
+          {images.map((image, index) => (
+            <PictureCard
+              key={URL.createObjectURL(image)}
+              src={URL.createObjectURL(image)}
+              index={index}
+              onDelete={(i) =>
+                setImages([...images.slice(0, i), ...images.slice(i + 1)])
+              }
+            />
+          ))}
+        </div>
+      </Card>
+
+      <Card className="p-4 grid grid-cols-1 md:grid-cols-3 m-6 gap-x-2">
+        <CardHeader className="col-span-3">
+          <h1 className="text-md font-semibold">Contact Information</h1>
+        </CardHeader>
+        <Input label="Owner Name" />
+        <Input label="Phone" />
+        <Input label="Email" />
+      </Card>
     </>
   );
 };
